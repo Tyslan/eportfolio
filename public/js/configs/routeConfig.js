1c7a6f331e462c7779392366cbc441f2d4a42a8d
Tyslan <verschingellorenz@hotmail.com>
2016-01-27 15:15:17 +0100

    navbar complete

--- public/js/configs/routeConfig.js
+++ public/js/configs/routeConfig.js
@@ -1,18 +1,15 @@
 // public/js/appRoutes.js
 angular
-    .module('PortfolioApp')
-    .config(['$stateProvider', '$urlRouterProvider', '$locationProvider',
-        function ($stateProvider, $urlRouterProvider, $locationProvider) {
+  .module('PortfolioApp')
+  .config(['$stateProvider', '$urlRouterProvider', '$locationProvider',
+    function ($stateProvider, $urlRouterProvider, $locationProvider) {
+      $stateProvider
+        .state('home', {
+          url: '/home',
+          templateUrl: '/views/home.html'
+        });
 
-        $stateProvider
-
-            // home page
-            .state('home', {
-                url: '/home',
-                templateUrl: '/views/home.html'
-            });
-
-        $urlRouterProvider.otherwise('home');
-        $locationProvider.html5Mode(true);
+      $urlRouterProvider.otherwise('home');
+      $locationProvider.html5Mode(true);
 
     }]);
